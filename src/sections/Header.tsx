2f05fa99e6fcf504c3f331d9004eb511e586126c
sango-neo <masangoneo@gmail.com>
2024-12-08 22:41:53 +0200

    fix dark mode header text

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 const Header = () => {
   return (
-    <section className="px-[5%] py-20  mx-auto bg-white">
+    <section className="px-[5%] py-20  mx-auto bg-ma_transBlue/5">
         <div className="container w-full mx-auto max-w-7xl">
-          <div className="w-full flex flex-col gap-6 md:flex-row md:justify-between max-w-5xl mx-auto">
+          <div className="w-full flex flex-col gap-6 md:flex-row md:justify-between max-w-5xl mx-auto text-[#444]">
             <div className="md:max-w-lg md:w-full">
-              <h1 className="text-[1.75rem] font-semibold text-[#444] tracking-tight leading-tight xl:text-[2rem]">An Asset Management Solutions System for <span className="text-ma_blue">Service Delivery</span> Excellence.</h1>
+              <h1 className="text-[1.75rem] font-semibold tracking-tight leading-tight xl:text-[2rem]">An Asset Management Solutions System for <span className="text-ma_blue">Service Delivery</span> Excellence.</h1>
             </div>
             <div className="md:max-w-xl md:w-full">
               <p>Moago Africa presents a cutting-edge, innovative system that provides a comprehensive solution for optimizing both movable and immovable assets while enhancing operational efficiency.</p>
